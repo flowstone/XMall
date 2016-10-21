d764468bf97f8d2386ad0552b667b6c7f0cd9dde
flowstone <yaoxue2014@hotmail.com>
2016-10-21 20:22:25 +0800

    创建后台Category

---
+++ Application/Back/Controller/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 尧
+ * Date: 10/21/2016
+ * Time: 2:54 PM
+ */
+
+namespace Back\Controller;
+
+use Think\Controller;
+
+class CategoryController extends Controller{
+
+    public function listAction(){
+
+    }
+}
